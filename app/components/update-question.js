0b748b4914e72eeca39553530215c77c8a8852ba
evan <stewarea@gmail.com>
2016-10-28 10:56:10 -0700

    update question save works

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+    showUpdateQuestion: false,
+    actions: {
+      showUpdateQuestion() {
+        this.set("showUpdateQuestion", true);
+      },
+      updateQuestion(question) {
+        var params = {
+          content: this.get('content'),
+          author: this.get('author'),
+          info: this.get('info')
+        };
+        this.set('showUpdateQuestion', false);
+        this.sendAction('updateQuestion', question, params);
+      }
+    }
 });
