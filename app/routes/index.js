579875442c0d69d1f426eadc814347fd8207b068
evan <stewarea@gmail.com>
2016-10-28 08:57:32 -0700

    move new question save info up to index

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,10 @@ export default Ember.Route.extend({
     return this.store.finalAll("question")
   },
   actions: {
-    
+    saveNewQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
   }
 });
