23e32cbdad68f283d54c9f6ef8ef9fd3f126f206
evan <stewarea@gmail.com>
2016-10-28 15:58:34 -0700

    attempted to make inputs required, failed miserably

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,15 @@
 /*jshint node:true*/
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    Ember.TextSupport.reopen({
+      attributeBindings: ["required"]
+  })
   });
+
+
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
